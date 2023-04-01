4051eb1587f6c00b303355a071e16ded87ed42c6
Sem-Alex <P08T.ya.ru>
2023-04-01 10:18:45 +0500

    three files instead of /small-lamp-rgb

---
+++ entity/recipe.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+    {
+    type = "recipe",
+    name = "small-lamp-rgb",
+    enabled = true,
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"small-lamp", 1}
+    },
+    result = "small-lamp-rgb"
+  }
+})
